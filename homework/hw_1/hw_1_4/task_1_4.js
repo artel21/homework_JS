0a4bff04e68aada0baa2ebf045670c2775ddc427
artel21xxx <temi4group@gmail.com>
2025-10-22 14:13:17 +0300

    hw_1 внесено зміни до task_1_4.js (додаткове)

--- homework/hw_1/hw_1_4/task_1_4.js
+++ homework/hw_1/hw_1_4/task_1_4.js
@@ -1,6 +1,9 @@
 //#ruUtWDUI
 
-let userName = prompt('What is your name?');
-console.log(userName);
+let firstName = prompt("Введіть ваше ім'я:");
+let middleName = prompt("Введіть ваше по-батькові:");
+let years = prompt("Введіть ваш вік (у роках):");
 
-
+console.log("Ім'я:" + firstName);
+console.log("По-батькові:" + middleName);
+console.log("Вік:" + years);
