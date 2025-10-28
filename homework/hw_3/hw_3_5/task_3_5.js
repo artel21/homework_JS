4ac1e777e36610f4c6f3f84cda504e54312eaeef
artel21xxx <temi4group@gmail.com>
2025-10-28 19:20:51 +0200

    hw_3 виконав ДЗ_3

---
+++ homework/hw_3/hw_3_5/task_3_5.js
@@ -0,0 +1,16 @@
+//#vLSZKMlO
+//
+// – Використовуючи данні з масиву, за допомоги document.write та циклу
+//
+// побудувати структуру за шаблоном
+
+
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+document.write(`<ul>`);
+for (let item of listOfItems) {
+    document.write(`<li>${item}</li>`);
+}
+document.write(`</ul>`);
+
+//====================================================================================
